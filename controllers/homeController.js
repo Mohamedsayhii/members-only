8114b8d967a33ba3aa508199bce4d8774eaaaaa6
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-11-26 23:58:08 +0100

    Add message form template

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,6 +2,10 @@ exports.getHomepage = (req, res) => {
 	res.render('homepage');
 };
 
+exports.getMessageForm = (req, res) => {
+	res.render('messageForm', { title: 'New Message' });
+};
+
 exports.getLogout = (req, res, next) => {
 	req.logout((err) => {
 		if (err) {
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
-const { getHomepage, getLogout } = require('../controllers/homeController');
+const {
+	getHomepage,
+	getLogout,
+	getMessageForm,
+} = require('../controllers/homeController');
 
 const homeRouter = Router();
 
 homeRouter.get('/', getHomepage);
+homeRouter.get('/new-message', getMessageForm);
+
 homeRouter.get('/logout', getLogout);
 
 module.exports = homeRouter;
